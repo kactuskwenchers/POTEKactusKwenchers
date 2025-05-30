70c9d26bf2c4335c58c7a9eb4dfa192be561b173
KactusKwenchers <hello@kactuskwenchers.com>
2025-05-30 12:34:31 -0700

    Initial Commit

---
+++ POTE/POTEApp.swift
@@ -0,0 +1,17 @@
+//
+//  POTEApp.swift
+//  POTE
+//
+//  Created by Kacrtus Kwenchers on 5/30/25.
+//
+
+import SwiftUI
+
+@main
+struct POTEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ POTETests/POTETests.swift
@@ -0,0 +1,17 @@
+//
+//  POTETests.swift
+//  POTETests
+//
+//  Created by Kacrtus Kwenchers on 5/30/25.
+//
+
+import Testing
+@testable import POTE
+
+struct POTETests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
